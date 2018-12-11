64d62936230b0268f96d4cc071067f6b8ddb15fe
Henrique Cabral <hcabral@veniam.com>
2018-12-11 21:01:54 +0000

    day11: improve running time for second half
    After looking around other solutions, it seems there's a known approach
    to this sort of problem based on partial sums over the whole grid
    (either that or everyone's just that much smarter than me!).  This cuts
    down the running time to around 1 s.

--- src/Day11.hs
+++ src/Day11.hs
@@ -3,38 +3,44 @@ module Day11 where
 import Data.List (foldl')
 import Data.Array.Unboxed (UArray, (!))
 import qualified Data.Array.Unboxed as U
+import qualified Data.Array as A
 
 type Coord = (Int, Int)
 type Power = Int
-type Grid = UArray Coord Power
 
 squareUpto :: Coord -> Int -> [Coord]
 squareUpto (x, y) u = U.range ((x, y), (x + u - 1, y + u - 1))
 
-fillCells :: Int -> Grid
+fillCells :: Int -> UArray Coord Power
 fillCells serial = U.array bounds [(c, power c) | c <- squareUpto (1, 1) 300]
     where power (x, y) = (x + 10) * ((x + 10) * y + serial) `div` 100 `mod` 10 - 5
           bounds = ((1, 1), (300, 300))
 
 maxSquareOf :: Int -> Int -> (Power, Coord)
-maxSquareOf serial sz = maximum [(at c, c) | c <- squareUpto (1,1) (300-sz+1)]
+maxSquareOf serial s = maximum [ (at c, c)
+                               | c <- U.range ((1,1), (300-s+1,300-s+1)) ]
   where grid = fillCells serial
-        at c = sum . map (grid !) $ squareUpto c sz
+        at c = sum . map (grid !) $ squareUpto c s
+
+combine :: A.Ix a => (a, a) -> [(a, a -> b)] -> [b]
+combine bs xs = map (\(p, f) -> f p) $ filter ((A.inRange bs) . fst) xs
 
 maxSquare :: Int -> (Coord, Int)
-maxSquare serial = let (_, _, (_, c, s)) = foldl' go start [1..299] in (c, s)
-  where grid = fillCells serial
-        empty = U.listArray (U.bounds grid) $ repeat 0
-        start = (grid, empty, (0, (0, 0), 0))
+maxSquare serial = snd $ maximum [ (at s c, (c, s))
+                                   | s <- [1..300]
+                                   , c <- U.range ((1,1), (300-s+1, 300-s+1)) ]
+  where ps = partials (fillCells serial)
+        at s (x, y) = sum . combine (A.bounds ps) $
+                          [ ((x + s - 1, y + s - 1), (ps A.!))
+                          , ((x - 1,     y + s - 1), negate . (ps A.!))
+                          , ((x + s - 1, y - 1),     negate . (ps A.!))
+                          , ((x - 1,     y - 1),     (ps A.!)) ]
 
-        go :: (Grid, Grid, (Power, Coord, Int)) -> Int -> (Grid, Grid, (Power, Coord, Int))
-        go (memo', memo, maxPower) sz = (memo'', memo', max maxPower maxPower')
-          where maxPower' = addSize $ maximum [(memo'' ! c, c) | c <- cells]
-                addSize (a, b) = (a, b, sz + 1)
-
-                memo'' = U.accum (+) memo' [(c, grow c) | c <- cells]
-                cells = squareUpto (1, 1) (300 - sz)
-                grow (x, y) = sum $ [ memo' ! (x + 1, y + 1)
-                                    , grid ! (x + sz, y)
-                                    , grid ! (x, y + sz)
-                                    , negate $ memo ! (x + 1, y + 1) ]
+partials :: UArray Coord Power -> A.Array Coord Power
+partials grid = ps
+  where ps = A.listArray (U.bounds grid) . map partial $ U.indices grid
+        partial (x, y) = sum . combine (U.bounds grid) $
+                             [ ((x,     y),     (grid !))
+                             , ((x - 1, y),     (ps A.!))
+                             , ((x,     y - 1), (ps A.!))
+                             , ((x - 1, y - 1), negate . (ps A.!)) ]
