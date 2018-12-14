f264462bde483d20da792e0efd6d9f2c901f3e9d
Henrique Cabral <hcabral@veniam.com>
2018-12-14 16:44:54 +0000

    day14: replace IntMap with Sequence
    Mostly just to try it out.  It seems to produce a modest speed
    improvement as well (around half a second).

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,29 +1,27 @@
 module Day14 (part1, part2) where
 
 import Data.List (tails, isPrefixOf)
-import Data.IntMap.Strict (IntMap)
-import qualified Data.IntMap.Strict as IM
+import Data.Sequence (Seq(..))
+import qualified Data.Sequence as Seq
 
 type Score = Int
 
-data Recipes = Recipes { getScores :: IntMap Score
-                       , getFocus :: (Int, Int)
-                       , getLength :: Int }
+data Recipes = Recipes { getScores :: Seq Score
+                       , getFocus :: (Int, Int) }
              deriving (Show)
 
 update :: Recipes -> ([Score], Recipes)
-update (Recipes scores (x, y) n) = (ns, Recipes scores' (x', y') n')
-  where scores' = foldr (\(i, s) m -> IM.insert i s m) scores $ zip [n..] ns
-        n' = n + length ns
+update (Recipes scores (x, y)) = (ns, Recipes scores' (x', y'))
+  where scores' = scores <> Seq.fromList ns
         (x', y') = (x `advance` sx, y `advance` sy)
         ns = let s = sx + sy
              in if s < 10 then [s] else [s `div` 10, s `mod` 10]
-        (sx, sy) = (scores IM.! x, scores IM.! y)
+        (sx, sy) = (scores `Seq.index` x, scores `Seq.index` y)
 
-        advance p d = (p + d + 1) `mod` n'
+        advance p d = (p + d + 1) `mod` length scores'
 
 stream :: [Score]
-stream = 3 : 7 : go (Recipes (IM.fromList [(0,3), (1,7)]) (0,1) 2)
+stream = 3 : 7 : go (Recipes (Seq.fromList [3,7]) (0,1))
   where go r = let (new, r') = update r in new ++ go r'
 
 part1 :: Int -> [Score]
